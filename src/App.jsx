9ac736e2e688ac56e6fe1b6ab1c82dbd24f420c1
Mayank Verma <mayankverma.dev@gmail.com>
2024-09-15 01:52:29 +0530

    added header Top

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import BackgroundImage from './Component/BackgroundImage/BackgroundImage'
 import Header from './Component/Header/Header'
 function App() {
   return (
     <>
-    <Header/>    
+    <Header/>
+    <BackgroundImage/>
   </>
   )
 }
