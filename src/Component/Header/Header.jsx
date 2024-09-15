fdee6e2500dfe8aabc45d5fb02c4d723c7a59350
Mayank Verma <mayankverma.dev@gmail.com>
2024-09-15 13:12:50 +0530

    added header Top

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -13,7 +13,7 @@ export default function Header() {
                     {/* mail Image */}
                     <div className='mail-div'>
                         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z"/></svg>
-                        <p className='ashish-Mail'>ashish@ashish.com</p>
+                        <p className='ashish-Mail'>info.divisense@gmail.com</p>
                     </div>
                     {/* time image */}
                     <div className='time-div'>
